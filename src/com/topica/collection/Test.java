5f2f9684599fef5afb9e7d3b00b51a065d20943f
EIE98VN <huynguyen052998@gmail.com>
2019-07-08 16:54:07 +0700

    add some stuff, not really matters

--- src/com/topica/collection/Test.java
+++ src/com/topica/collection/Test.java
@@ -9,18 +9,22 @@ import java.util.Collections;
 public class Test {
 	private static final float PI = 3.14f;
 	private static final int MAX = 99999999;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void printMenu() {
 		String menu = "1./Find the closest are\n2./Exit";
+		System.out.println(menu);
+		System.out.println("Enter your choice");
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		float min = MAX;
 		float radius;
-		int choice; 
-		
+		int choice;
+		float area;
 		ArrayList<Circle> circleList = new ArrayList<>();
 		ArrayList<Integer> listIndex = new ArrayList<>();
-		
-		float area;
 		BufferedReader reader = null;
-		
+
 		for (int i = 0; i < 100; i++) {
 			Circle circle = new Circle();
 			circleList.add(circle);
@@ -31,39 +35,38 @@ public class Test {
 		for (Circle circle : circleList) {
 			System.out.println(circle);
 		}
-		
+
 		do {
-			System.out.println(menu);
-			System.out.println("Enter your choice");
+			printMenu();
 			reader = new BufferedReader(new InputStreamReader(System.in));
 			choice = Integer.valueOf(reader.readLine());
-			
 			switch (choice) {
 			case 1:
 				do {
 					System.out.println("Enter area: ");
-					reader = new BufferedReader(new InputStreamReader(System.in)); 
+					reader = new BufferedReader(new InputStreamReader(System.in));
 					area = Float.valueOf(reader.readLine());
 				} while (area <= 0);
-				
+
 				radius = (float) Math.sqrt(area / PI);
-				
+
 				for (int i = 0; i < circleList.size(); i++) {
 					float dis = Math.abs(radius - circleList.get(i).getRadius());
-					if(dis==min) listIndex.add(i);
+					if (dis == min)
+						listIndex.add(i);
 					if (dis < min) {
 						listIndex.clear();
 						listIndex.add(i);
-						min = dis;		
+						min = dis;
 					}
 				}
 
-				System.out.println("Index: " + listIndex + " Area: "+ circleList.get(listIndex.get(0)).computeArea());
+				System.out.println("Index: " + listIndex + " Area: " + circleList.get(listIndex.get(0)).computeArea());
 				break;
 
 			default:
 				break;
 			}
-		} while (choice!=2);
+		} while (choice != 2);
 	}
 }
